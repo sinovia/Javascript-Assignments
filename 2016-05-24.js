f0034e23bad244a0163f6c39cba25d9b653ee594
SINOVIA <somayfield@gmail.com>
2016-05-24 10:33:35 -0400

    Corrected problem

--- 2016-05-24.js
+++ 2016-05-24.js
@@ -1,7 +1,19 @@
-var numberofChildren prompt ("How many children do you have?")
+//var numberofChildren prompt ("How many children do you have?")
 
-var partnerName prompt ("What is your partner's name?")
+//var partnerName prompt ("What is your partner's name?")
 
-var numberofChildren prompt ("Where do you want to live?")
+//var numberofChildren prompt ("Where do you want to live?")
 
-var job title prompt ("Where do you want to live?")
+//var job title prompt ("Where do you want to live?")
+
+var numberofChildren= 2
+
+var partnerName = "Jim"
+
+var locate = "Little Rock"
+
+var jobTitle = "Software Developer"
+
+var fortune = "You will be a " + jobTitle + " in" + " " + locate + " " + "and married to " + partnerName + " " + "with " + numberofChildren + " " + "kids"
+
+console.log(fortune);
